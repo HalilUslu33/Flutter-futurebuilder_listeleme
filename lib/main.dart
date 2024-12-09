20e6527cba30ec165bdfd983c15861606b1eb7cb
HalilUslu33 <142406737+HalilUslu33@users.noreply.github.com>
2024-12-10 00:57:00 +0300

    futurebuilder_listeleme

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  Future<List<String>> verileriGetir() async {
+    var ulkeListesi = ["Türkiye","Almanya","İtalya","Rusya","Çin"];
+
+    return ulkeListesi;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: FutureBuilder<List<String>>(
+        future: verileriGetir(),
+        builder: (context,snapshot){
+          if(snapshot.hasData){
+
+            var ulkeListesi = snapshot.data;
+
+            return ListView.builder(
+              itemCount: ulkeListesi!.length,
+              itemBuilder: (context,indeks){
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      height: 50,
+                      child: Row(
+                        children: [
+                          Text(ulkeListesi[indeks]),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+
+          }else{
+            return Center();
+          }
+        },
+      ),
+
+    );
+  }
+}
